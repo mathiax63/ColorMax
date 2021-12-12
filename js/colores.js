d106fb31b14a1e4ae0a309988db90506f5b20ead
mathiax63 <mathyoyo@hotmail.es>
2021-12-12 19:20:11 -0300

    subo mejoras

--- js/colores.js
+++ js/colores.js
@@ -2,28 +2,28 @@
 window.addEventListener("load", function () {
     //console.log("entre")
  let colores = []
-
- colores[0] =  "#ff708215"
- colores[1] =  "#ff402045"
- colores[2] =  "#ff504046"
- colores[3] =  "#ff801056"
- colores[4] =  "#ff048075"
- colores[5] =  "#ff962078"
- colores[6] =  "#ff804507"
- colores[7] =  "#ff301406"
- colores[8] = "FFFF0099"
+ colores[0] =""
+ colores[1] =  "#000066"
+ colores[2] =  "#ff402045"
+ colores[3] =  "#ff504046"
+ colores[4] =  "#ff801056"
+ colores[5] =  "#99FF99"
+ colores[6] =  "#000000"
+ colores[7] =  "#CC3300"
+ colores[8] =  "#FF6666"
+ colores[9] = "#FFF56199"
 
  let coloresNombres = []
-
- coloresNombres[0] =  " El color RGB que esta viendo es #ff708215"
- coloresNombres[1] =  "El color RGB que esta viendo es #ff402045"
- coloresNombres[2] =  "El color RGB que esta viendo es #ff504046"
- coloresNombres[3] =  "El color RGB que esta viendo es #ff801056"
- coloresNombres[4] =  "El color RGB que esta viendo es #ff048075"
- coloresNombres[5] =  "El color RGB que esta viendo es #ff962078"
- coloresNombres[6] =  "El color RGB que esta viendo es #ff804507"
- coloresNombres[7] =  "El color RGB que esta viendo es #ff301406"
- coloresNombres[8] = "El color RGB que esta viendo es FFFF0099"
+ coloresNombres[0] =""
+ coloresNombres[1] =  "#000066"
+ coloresNombres[2] =  "#ff402045"
+ coloresNombres[3] =  "#ff504046"
+ coloresNombres[4] =  "#ff801056"
+ coloresNombres[5] =  "#99FF99"
+ coloresNombres[6] =  "#000000"
+ coloresNombres[7] =  "#CC3300"
+ coloresNombres[8] =  "#FF6666"
+ coloresNombres[9] = "#FFF56199"
 
  let coloresDelDiv = document.getElementById("divDeColores")
 
@@ -33,7 +33,7 @@ window.addEventListener("load", function () {
  function corredorDeColores(){
      //console.log("pase?")
      
-  if(contador>8){
+  if(contador>=8){
     contador=0
   }
      contador++
@@ -54,7 +54,7 @@ pColor.style.color = colores[0]
 
  let contadorp = 0
  function cambioDeP(){
-    if(contadorp>8){
+    if(contadorp>=8){
         contadorp=0
       }
      contadorp++
@@ -69,12 +69,12 @@ pColor.innerHTML= coloresNombres[0]
 
  let contadorpn = 0
  function cambioDeDatosP(){
-    if(contadorpn>8){
+    if(contadorpn>=8){
         contadorpn=0
       }
      contadorpn++
      pColor.innerHTML = coloresNombres[contadorpn]
-     pColor.innerHTML = coloresNombres[contadorpn]
+     pColor.innerHTML = coloresNombres[contadorpn]   
  }
  setInterval(cambioDeDatosP, 2000)
 
@@ -96,18 +96,20 @@ pColor.innerHTML= coloresNombres[0]
    let sumaDeAmbos =asterisco+resultado
    
    return sumaDeAmbos
-   console.log(sumaDeAmbos)
+   //console.log(sumaDeAmbos)
 
  }
  function resultadoDeAzar (){
   
  let colorAleatoreo = document.getElementById("divDeAleatoreo")
+ let sumaConLasP = document.getElementById("pSumaDeAmbos")
  colorAleatoreo.style.backgroundColor = numeroAlAzar(8);
+ sumaConLasP.innerHTML = "El color rgb que esta viendo es: " + numeroAlAzar(8)
      
 }
     
 
-setInterval(resultadoDeAzar, 1000)
+setInterval(resultadoDeAzar, 3000)
 
 
 console.log(numeroAlAzar(8));
